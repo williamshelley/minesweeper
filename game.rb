7ddbe884aaca18552a232241fc6d9554264f7335
William Shelley <wjshelley5018@gmail.com>
2020-05-28 16:15:11 -0400

    Added load and save games

--- game.rb
+++ game.rb
@@ -1,23 +1,50 @@
+require "fileutils"
+require "yaml"
 require_relative "board"
 require_relative "player"
 
 class Game
+  GAMES_DIR = "game_history"
+
   def initialize(size = 9)
     @board = Board.new(size)
+    FileUtils.mkdir_p(GAMES_DIR)
+  end
+
+  def save_game(output_file)
+    File.open("./" + GAMES_DIR + "/" + output_file + ".yaml", "w") do |file|
+      file.write(@board.to_yaml)
+    end
+  end
+
+  def load_game(file)
+    board = nil
+    File.open("./" + GAMES_DIR + "/" + file + ".yaml", "r") do |file|
+      board = YAML.load(file.read())
+    end
+    @board = board if board
   end
 
   def run
     cmd = nil
     while cmd != "exit" && !@board.gameover?
       @board.render
-      cmd, pos = Player.get_cmd_and_pos
+      print "enter a command (cmd *args): "
+      cmd, *args = gets.chomp.split(" ")
       case cmd
       when "reveal"
+        pos = args[0].split(",").map(&:to_i)
         @board.reveal(pos)
       when "flag"
+        pos = args[0].split(",").map(&:to_i)
         @board.flag(pos)
+      when "save"
+        self.save_game(args.join("-"))
+      when "load"
+        self.load_game(args[0])
       end
     end
+
     puts "GAME OVER"
   end
 end
